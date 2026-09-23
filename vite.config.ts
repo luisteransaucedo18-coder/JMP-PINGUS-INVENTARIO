b2bb56e44497d3b62f72e73da25f531d60321dac
Luis Teran <luisteransaucedo18@gmail.com>
2026-09-23 12:30:51 -0500

    jejej

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig(({ mode }) => {
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
+      chunkSizeWarningLimit: 800,
     },
     plugins: [
 react(),
